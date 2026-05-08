53cd9486a47d804667a0ec9151c06bf1637ae580
Komal More <komalmore31724@gmail.com>
2026-05-08 22:02:40 +0530

    advance concepts in req

--- src/app.js
+++ src/app.js
@@ -2,15 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.use('/test',(req,res)=>{
-    res.send("Testing page")
+
+app.get('/user/:userId/:userName/:city',(req,res)=>{
+    // http://localhost:7777/user?userName=komal&city=pune
+    // const params=req.query;
+    const params= req.params;
+    res.send(params)
+})
+app.post('/user',(req,res)=>{
+    res.send("post res")
+})
+app.delete('/user',(req,res)=>{
+    res.send("delete res")
+})
+app.patch('/user',(req,res)=>{
+    res.send("patch res")
 })
 
-
-app.use('/',(req,res)=>{
-    res.send("Home page")
+app.get(/.*fig$/,(req,res)=>{
+    res.send("profile res")
 })
-
 
 app.listen(7777,()=>{
     console.log("server listening")
